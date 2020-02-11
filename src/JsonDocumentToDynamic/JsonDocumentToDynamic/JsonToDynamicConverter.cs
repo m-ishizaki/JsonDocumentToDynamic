dc94d05c4e34a3941ac5b1b04a2e8236cf2764c2
m ishizaki <love.ms.is12t@hotmail.co.jp>
2020-02-12 02:24:58 +0900

    Initial commit.

---
+++ src/JsonDocumentToDynamic/JsonDocumentToDynamic/JsonToDynamicConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace Rksoftware.JsonDocumentToDynamic
+{
+    public static class JsonToDynamicConverter
+    {
+        public static ExpandoObject Parse(JsonElement element) => toExpandoObject(element);
+
+        public static ExpandoObject Parse(JsonDocument document) => toExpandoObject(document.RootElement);
+
+        public static ExpandoObject Parse(string json, JsonDocumentOptions options = default)
+        {
+            using var document = JsonDocument.Parse(json, options);
+            return toExpandoObject(document.RootElement);
+        }
+
+        static object propertyValue(JsonElement elm) =>
+            elm.ValueKind switch
+            {
+                JsonValueKind.Null => null,
+                JsonValueKind.Number => elm.GetDecimal(),
+                JsonValueKind.String => elm.GetString(),
+                JsonValueKind.False => false,
+                JsonValueKind.True => true,
+                JsonValueKind.Array => elm.EnumerateArray().Select(m => propertyValue(m)).ToArray(),
+                _ => toExpandoObject(elm),
+            };
+
+        static ExpandoObject toExpandoObject(JsonElement elm) =>
+            elm.EnumerateObject()
+            .Aggregate(
+                new ExpandoObject(),
+                (exo, prop) => { ((IDictionary<string, object>)exo).Add(prop.Name, propertyValue(prop.Value)); return exo; });
+    }
+}
---
+++ src/JsonDocumentToDynamic/Run/Program.cs
@@ -0,0 +1,26 @@
+﻿using Rksoftware.JsonDocumentToDynamic;
+using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = args.FirstOrDefault();
+            Console.WriteLine(json);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                Console.WriteLine("null");
+                return;
+            }
+
+            var expandoObject = JsonToDynamicConverter.Parse(json);
+
+            Console.WriteLine(JsonSerializer.Serialize(expandoObject));
+        }
+    }
+}
